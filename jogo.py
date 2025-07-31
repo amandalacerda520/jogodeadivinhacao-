b63db40eea15515df611e312da42d98b706d4688
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-31 14:49:53 +0000

    parte2dojogo

--- jogo.py
+++ jogo.py
@@ -7,5 +7,17 @@ print("            ✨           ")
 
 numerossecreto= random.randrange(0,100)
 totaltentativas = 0 
-PONTOS
+ponto = 100
+
+print("qual niveis de dificuldade?")
+print("(1)-fácil (2)-médio (3)-difícil  ")
+
+nivel = int(input("escolha um nível "))
+
+if(nivel == 1 ):
+    print("20 tentativas")
+    totaltentativas = 20
+elif(nivel == 2):
+    prin
+
 
