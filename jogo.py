fe1e5c05f1064df841590fed55243088076498c3
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-31 12:17:51 +0000

    PRIMEIRA PARTE DO JOGO

---
+++ jogo.py
@@ -0,0 +1,11 @@
+import random
+print("#########################")
+print("#  jogo de adivinhação  #")
+print("#        ✨amanda✨     #")
+print("#########################")
+print("            ✨           ")
+
+numerossecreto= random.randrange(0,100)
+totaltentativas = 0 
+PONTOS
+
